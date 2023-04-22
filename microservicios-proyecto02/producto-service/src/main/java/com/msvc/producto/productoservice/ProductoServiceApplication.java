c195966bc54e905a405222c677aac4a1118a41e6
horacio ortega <63156418+horacio186@users.noreply.github.com>
2023-04-21 22:33:16 -0400

    Update ProductoServiceApplication.java

--- microservicios-proyecto02/producto-service/src/main/java/com/msvc/producto/productoservice/ProductoServiceApplication.java
+++ microservicios-proyecto02/producto-service/src/main/java/com/msvc/producto/productoservice/ProductoServiceApplication.java
@@ -3,10 +3,9 @@ package com.msvc.producto.productoservice;
 import com.msvc.producto.productoservice.repository.ProductoRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
 
 @SpringBootApplication
-@EnableMongoRepositories(basePackageClasses = ProductoRepository.class)
 public class ProductoServiceApplication {
 
 	public static void main(String[] args) {
